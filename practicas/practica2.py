e1350d431ba0d33e98a22f0d27a956d7df759182
theboque <dedgar2335@gmail.com>
2022-08-11 15:59:03 -0500

    Añadida la practica 2 del modulo de analisis de datos de edc

---
+++ practicas/practica2.py
@@ -0,0 +1,56 @@
+# 2.1 Ejercicio 1 (1.5 puntos)
+# Escribir un programa que convierta un valor dado en grados Fahrenheit a grados
+# Celsius.
+
+fahrenheit = float(input('Introduce los grados fahrenheit: '))
+
+celcius = (fahrenheit - 32) * (5 / 9)
+
+print(f'{fahrenheit} grados fahrenheit es igual a {celcius:.2f} grados celcius ')
+
+
+# 2.2 Ejercicio 2 (1.5 puntos)
+# Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.
+
+# num1 = float(input('Introduce un numero: '))
+# num2 = float(input('Introduce otro numero: '))
+
+# print(f'La suma de {num1} + {num2} es --->', num1 + num2)
+# print(f'La resta de {num1} - {num2} es --->', num1 - num2)
+# print(f'La multiplicacion de {num1} * {num2} es --->', num1 * num2)
+# if num2 == 0:
+#     print('No se puede dividir entre cero!!!!!!!!')
+# else:
+#     print(f'La divicion de {num1} + {num2} es ---> {(num1 / num2):.2}')
+
+
+# 2.3 Ejercicio 3 (1.5 puntos)
+# Calcular el perímetro y área de un rectángulo dada su base y su altura.
+
+# base_rectangulo = float(input('Introduce el la base del rectangulo: '))
+# altura_rectangulo = float(input('Introduce el la altura del rectangulo: '))
+
+# perimetro =  2 * base_rectangulo * altura_rectangulo
+# area =  base_rectangulo * altura_rectangulo
+
+# print(f'El area del recutagulo con las dimenciones que ingresante es: {area}')
+# print(f'El perimetro del recutagulo con las dimenciones que ingresante es: {perimetro}')
+
+# Calcular la media de tres números pedidos por teclado
+# num1 = float(input("Introduce el número 1:"))
+# num2 = float(input("Introduce el número 2:"))
+# num3 = float(input("Introduce el número 3:"))
+# media = (num1 + num2 +num3) / 3
+# print("La media es:", media)
+
+
+
+
+
+
+
+
+
+
+
+
